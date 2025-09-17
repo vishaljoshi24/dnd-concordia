c5dd2b46157efd17cfec08716cf08625c65aeabf
Vishal Joshi <we19383@bristol.ac.uk>
2025-09-17 14:44:45 +0000

    Modified dnd_player prefab

--- concordia/prefabs/entity/dnd_player.py
+++ concordia/prefabs/entity/dnd_player.py
@@ -77,7 +77,7 @@ class Entity(prefab_lib.Prefab):
     """
     entity_name = self.params.get('name', '')
     entity_gender = self.params.get('gender', '')
-    entity_class = self.params.get('class', '')
+    entity_character_class = self.params.get('character class', '')
     entity_level = self.params.get('level', int)
     entity_race = self.params.get('race', '')
     entity_background = self.params.get('background', '')
@@ -86,7 +86,6 @@ class Entity(prefab_lib.Prefab):
     entity_armour_class = self.params.get('armour_class', int)
     entity_ability_score = self.params.get('ability_score', dict)
     entity_ability_modifier = self.params.get('ability_modifier', dict)
-    entity_initiative = self.params.get('initiative', int)
     entity_speed = self.params.get('speed', int)
     entity_context = self.params.get('context', '')
     entity_traits = self.params.get('traits', '')
@@ -95,11 +94,10 @@ class Entity(prefab_lib.Prefab):
     entity_flaws = self.params.get('flaws', '')
     entity_goal = self.params.get('goal', '')
 
-
     instructions_key = 'Instructions'
     instructions = agent_components.dnd_instructions.Instructions(
         agent_name=entity_name,
-        pre_act_label='\nInstructions',
+        pre_act_label='\nD&D Instructions',
     )
 
     observation_to_memory_key = 'Observation'
@@ -182,6 +180,150 @@ class Entity(prefab_lib.Prefab):
       goal_key = None
       overarching_goal = None
 
+    if entity_gender:
+      gender_key = 'gender'
+      gender = agent_components.constant.Constant(
+          state=entity_gender, pre_act_label='\nGender'
+      )
+    else:
+      gender_key = None
+      gender = None
+
+    if entity_character_class:
+      character_class_key = 'class'
+      character_class = agent_components.constant.Constant(
+          state=entity_character_class, pre_act_label='\nCharacter Class'
+      )
+    else:
+      character_class_key = None
+      character_class = None
+
+    if entity_level:
+      level_key = 'level'
+      level = agent_components.constant.Constant(
+          state=entity_level, pre_act_label='\nLevel'
+      )
+    else:
+      level_key = None
+      level = None
+
+    if entity_race:
+      race_key = 'race'
+      race = agent_components.constant.Constant(
+          state=entity_race, pre_act_label='nRace'
+      )
+    else:
+      race_key = None
+      race = None
+
+    if entity_background:
+      background_key = 'background'
+      background = agent_components.constant.Constant(
+          state=entity_background, pre_act_label='\nBackground'
+      )
+    else:
+      background_key = None
+      background = None
+
+    if entity_alignment:
+      alignment_key = 'alignment'
+      alignment = agent_components.constant.Constant(
+          state=entity_alignment, pre_act_label='\nAlignment'
+      )
+    else:
+      alignment_key = None
+      alignment = None
+
+    if entity_equipment:
+      equipment_key = 'equipment'
+      equipment = agent_components.constant.Constant(
+          state=entity_equipment, pre_act_label='\nEquipment'
+      )
+    else:
+      equipment_key = None
+      equipment = None
+
+    if entity_armour_class:
+      armour_class_key = 'armour_class'
+      armour_class = agent_components.constant.Constant(
+          state=entity_armour_class, pre_act_label='\nArmour Class'
+      )
+    else:
+      armour_class_key = None
+      armour_class = None
+
+    if entity_ability_score:
+      ability_score_key = 'ability_score'
+      ability_score = agent_components.constant.Constant(
+          state=entity_ability_score, pre_act_label='\nAbility Score'
+      )
+    else:
+      ability_score_key = None
+      ability_score = None
+
+    if entity_ability_modifier:
+      ability_modifier_key = 'ability_modifier'
+      ability_modifier = agent_components.constant.Constant(
+          state=entity_ability_modifier, pre_act_label='\nAbility Modifier'
+      )
+    else:
+      ability_modifier_key = None
+      ability_modifier = None
+
+    if entity_speed:
+      speed_key = 'speed'
+      speed = agent_components.constant.Constant(
+          state=entity_speed, pre_act_label='\nSpeed'
+      )
+    else:
+      speed_key = None
+      speed = None
+
+    if entity_context:
+      context_key = 'context'
+      context = agent_components.constant.Constant(
+          state=entity_context, pre_act_label='\nContext'
+      )
+    else:
+      context_key = None
+      context = None
+
+    if entity_traits:
+      traits_key = 'traits'
+      traits = agent_components.constant.Constant(
+          state=entity_traits, pre_act_label='\nTraits'
+      )
+    else:
+      traits_key = None
+      traits = None
+
+    if entity_bonds:
+      bonds_key = 'bonds'
+      bonds = agent_components.constant.Constant(
+          state=entity_bonds, pre_act_label='\nBonds'
+      )
+    else:
+      bonds_key = None
+      bonds = None
+
+    if entity_flaws:
+      flaws_key = 'flaws'
+      flaws = agent_components.constant.Constant(
+          state=entity_flaws, pre_act_label='\nFlaws'
+      )
+    else:
+      flaws_key = None
+      flaws = None
+
+    if entity_ideals:
+      ideals_key = 'ideals'
+      ideals = agent_components.constant.Constant(
+          state=entity_ideals, pre_act_label='\nIdeals'
+      )
+    else:
+      ideals_key = None
+      ideals = None
+
     plan_key = 'Plan'
     plan = agent_components.plan.Plan(
         model=model,
@@ -217,6 +359,70 @@ class Entity(prefab_lib.Prefab):
       components_of_agent[goal_key] = overarching_goal
       # Place goal after the instructions.
       component_order.insert(1, goal_key)
+
+    if gender is not None:
+      components_of_agent[gender_key] = gender
+      component_order.insert(2, gender_key)
+
+    if character_class is not None:
+      components_of_agent[character_class_key] = character_class
+      component_order.insert(3, character_class_key)
+
+    if level is not None:
+      components_of_agent[level_key] = level
+      component_order.insert(4, level_key)
+
+    if race is not None:
+      components_of_agent[race_key] = race
+      component_order.insert(5, race_key)
+
+    if background is not None:
+      components_of_agent[background_key] = background
+      component_order.insert(6, background_key)
+
+    if alignment is not None:
+      components_of_agent[alignment_key] = background
+      component_order.insert(7, alignment_key)
+
+    if equipment is not None:
+      components_of_agent[equipment_key] = equipment
+      component_order.insert(8, equipment_key)
+
+    if armour_class is not None:
+      components_of_agent[armour_class] = armour_class
+      component_order.insert(9, armour_class_key)
+
+    if ability_score is not None:
+      components_of_agent[ability_score] = ability_score
+      component_order.insert(10, ability_score_key)
+
+    if ability_modifier is not None:
+      components_of_agent[ability_modifier] = ability_modifier
+      component_order.insert(11, ability_modifier_key)
+
+    if speed is not None:
+      components_of_agent[speed] = speed
+      component_order.insert(12, speed_key)
+
+    if context is not None:
+      components_of_agent[context] = context
+      component_order.insert(13, context_key)
+
+    if traits is not None:
+      components_of_agent[traits] = traits
+      component_order.insert(14, traits_key)
+
+    if bonds is not None:
+      components_of_agent[bonds] = bonds
+      component_order.insert(15, bonds_key)
+
+    if flaws is not None:
+      components_of_agent[flaws] = flaws
+      component_order.insert(16, flaws_key)
+
+    if ideals is not None:
+      components_of_agent[ideals] = ideals
+      component_order.insert(17, ideals_key)
 
     act_component = agent_components.concat_act_component.ConcatActComponent(
         model=model,
