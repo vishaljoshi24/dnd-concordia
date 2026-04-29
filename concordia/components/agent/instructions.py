8553e069896a272c214c43eb5e98a7e2baf08b37
Vishal Joshi <146184402+vishaljoshi24@users.noreply.github.com>
2026-04-29 13:46:46 +0100

    Update instructions.py
    Added alternative dnd instructions

--- concordia/components/agent/instructions.py
+++ concordia/components/agent/instructions.py
@@ -28,16 +28,28 @@ class Instructions(constant.Constant):
       pre_act_label: str = DEFAULT_INSTRUCTIONS_PRE_ACT_LABEL,
   ):
     state = (
+        # f'The instructions for how to play the role of {agent_name} are as '
+        # 'follows. This is a social science experiment studying how well you '
+        # f'play the role of a character named {agent_name}. The experiment '
+        # 'is structured as a tabletop roleplaying game (like dungeons and '
+        # 'dragons). However, in this case it is a serious social science '
+        # 'experiment and simulation. The goal is to be realistic. It is '
+        # f'important to play the role of a person like {agent_name} as '
+        # f'accurately as possible, i.e., by responding in ways that you think '
+        # f'it is likely a person like {agent_name} would respond, and taking '
+        # f'into account all information about {agent_name} that you have. '
+        # 'Always use third-person limited perspective.'
         f'The instructions for how to play the role of {agent_name} are as '
-        'follows. This is a social science experiment studying how well you '
+        'follows. This is a short scenario in which you '
         f'play the role of a character named {agent_name}. The experiment '
         'is structured as a tabletop roleplaying game (like dungeons and '
-        'dragons). However, in this case it is a serious social science '
-        'experiment and simulation. The goal is to be realistic. It is '
+        'dragons). The goal is to be consistent, but creative. It is '
         f'important to play the role of a person like {agent_name} as '
         f'accurately as possible, i.e., by responding in ways that you think '
         f'it is likely a person like {agent_name} would respond, and taking '
         f'into account all information about {agent_name} that you have. '
-        'Always use third-person limited perspective.'
+        'It is important that you collaborate with with the other players'
+        'on the task at hand and follow the Game Master instructions.'
+        'Always use first-person limited perspective.'
     )
     super().__init__(state=state, pre_act_label=pre_act_label)
